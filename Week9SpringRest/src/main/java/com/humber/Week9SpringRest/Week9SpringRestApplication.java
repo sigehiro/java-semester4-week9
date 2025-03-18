497505201a163ebd0e0dd1aaf82ff840de307ac8
sigehiro <sigehiro.0728@gmail.com>
2025-03-18 18:36:12 -0400

    1st commit

---
+++ Week9SpringRest/src/main/java/com/humber/Week9SpringRest/Week9SpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.humber.Week9SpringRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week9SpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week9SpringRestApplication.class, args);
+	}
+
+}
---
+++ Week9SpringRest/src/test/java/com/humber/Week9SpringRest/Week9SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.Week9SpringRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week9SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
